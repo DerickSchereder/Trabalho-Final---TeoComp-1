ef5e329828418087fb224e844fe3ee2592de8daf
Manamin-min <manamiadate@gmail.com>
2026-06-16 18:43:26 -0300

    Adiciona suporte a categorias food e toxic e as fases

--- src/analisador.py
+++ src/analisador.py
@@ -7,7 +7,7 @@ import ply.yacc as yacc
 reserved = {
     'route': 'ROUTE', 'begin': 'BEGIN', 'end': 'END',
     'load': 'LOAD', 'unload': 'UNLOAD', 'sail': 'SAIL', 'to': 'TO',
-    'heavy': 'HEAVY', 'light': 'LIGHT',
+    'heavy': 'HEAVY', 'light': 'LIGHT', 'food': 'FOOD', 'toxic': 'TOXIC',
     'weight': 'WEIGHT', 'kg': 'KG'
 }
 
@@ -76,6 +76,54 @@ def p_fase_light_sail(p):
 def p_fase_light_empty(p):
     '''fase_light : empty'''
     p[0] = []
+
+def p_fase_light_food(p):
+    '''fase_light : fase_food'''
+    p[0] = p[1]
+
+def p_fase_light_toxic(p):
+    '''fase_light : fase_toxic'''
+    p[0] = p[1]
+
+def p_fase_food_empty(p):
+    '''fase_food : empty'''
+    p[0] = []
+
+def p_fase_toxic_empty(p):
+    '''fase_toxic : empty'''
+    p[0] = []
+
+def p_fase_food_load_food(p):
+    '''fase_food : LOAD FOOD NOME WEIGHT PESO_LEVE KG fase_food UNLOAD FOOD fase_food'''
+    load_cmd = {'action': 'load', 'type': 'food', 'name': p[3], 'weight': p[5]}
+    unload_cmd = {'action': 'unload', 'type': 'food', 'name': p[3], 'weight': p[5]}
+    p[0] = [load_cmd] + p[7] + [unload_cmd] + p[10]
+
+def p_fase_toxic_load_toxic(p):
+    '''fase_toxic : LOAD TOXIC NOME WEIGHT PESO_LEVE KG fase_toxic UNLOAD TOXIC fase_toxic'''
+    load_cmd = {'action': 'load', 'type': 'toxic', 'name': p[3], 'weight': p[5]}
+    unload_cmd = {'action': 'unload', 'type': 'toxic', 'name': p[3], 'weight': p[5]}
+    p[0] = [load_cmd] + p[7] + [unload_cmd] + p[10]
+
+def p_fase_food_load_light(p):
+    '''fase_food : LOAD LIGHT NOME WEIGHT PESO_LEVE KG fase_food UNLOAD LIGHT fase_food'''
+    load_cmd = {'action': 'load', 'type': 'light', 'name': p[3], 'weight': p[5]}
+    unload_cmd = {'action': 'unload', 'type': 'light', 'name': p[3], 'weight': p[5]}
+    p[0] = [load_cmd] + p[7] + [unload_cmd] + p[10]
+
+def p_fase_food_sail(p):
+    '''fase_food : SAIL TO NOME fase_food'''
+    p[0] = [{'action': 'sail', 'dest': p[3]}] + p[4]
+
+def p_fase_toxic_load_light(p):
+    '''fase_toxic : LOAD LIGHT NOME WEIGHT PESO_LEVE KG fase_toxic UNLOAD LIGHT fase_toxic'''
+    load_cmd = {'action': 'load', 'type': 'light', 'name': p[3], 'weight': p[5]}
+    unload_cmd = {'action': 'unload', 'type': 'light', 'name': p[3], 'weight': p[5]}
+    p[0] = [load_cmd] + p[7] + [unload_cmd] + p[10]
+
+def p_fase_toxic_sail(p):
+    '''fase_toxic : SAIL TO NOME fase_toxic'''
+    p[0] = [{'action': 'sail', 'dest': p[3]}] + p[4]
 
 # --- VAZIO E ERRO ---
 def p_empty(p):
